05196c68ba34c7f0dde1000b5b2da7f4c35a3346
kCox96 <33167621+kCox96@users.noreply.github.com>
2017-10-28 02:22:26 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+//Kiana cox
+//150346658
+//08/05/2017
+
+//----This class runs the browser----
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Toolbar main = new Toolbar();
+      main.setVisible(true);
+	}
+
+}
